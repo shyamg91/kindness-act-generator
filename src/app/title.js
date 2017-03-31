613d560b8f22babb287f97df06ad32974ca0d3f9
shyamg91 <shyamg7777@gmail.com>
2017-03-31 20:55:12 +1300

    Changed fonts and functions

--- src/app/title.js
+++ src/app/title.js
@@ -6,15 +6,21 @@ module.exports = {
 function kindnessController($http, $log) {
   $log.debug('Kindness');
   var vm = this;
-
-  $http
-    .get('./app/techs/kindnessMessages.json')
-    .then(function (response) {
-      $log.debug('here', response);
-      var random = Math.floor(Math.random() * 5) + 1;
-      vm.kindnessMessage = response.data[random];
-      $log.debug(random, response.data[random]);
-    }, function (err) {
-      $log.debug('err', err);
-    });
+  vm.getAct = function () {
+    $http
+      .get('./app/techs/kindnessMessages.json')
+      .then(function (response) {
+        $log.debug('here', response);
+        var random = Math.floor(Math.random() * 5) + 1;
+        vm.kindnessMessage = response.data[random];
+        vm.url = 'https://twitter.com/intent/tweet?text=I did a random act of kindness today. ' + vm.kindnessMessage.message + '%20%23happyworld';
+        $log.debug(random, response.data[random]);
+      }, function (err) {
+        $log.debug('err', err);
+      });
+  };
+  vm.getAct();
+  vm.newAct = function () {
+    vm.getAct();
+  };
 }
