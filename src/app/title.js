59b13d79f44fdd19c66c78939379b4d69cb269d8
shyamg91 <shyamg7777@gmail.com>
2017-03-31 21:15:00 +1300

    Animations

--- src/app/title.js
+++ src/app/title.js
@@ -3,21 +3,27 @@ module.exports = {
   controller: kindnessController
 };
 
-function kindnessController($http, $log) {
+function kindnessController($http, $log, $timeout) {
   $log.debug('Kindness');
   var vm = this;
   vm.getAct = function () {
-    $http
-      .get('./app/techs/kindnessMessages.json')
-      .then(function (response) {
-        $log.debug('here', response);
-        var random = Math.floor(Math.random() * 5) + 1;
-        vm.kindnessMessage = response.data[random];
-        vm.url = 'https://twitter.com/intent/tweet?text=I did a random act of kindness today. ' + vm.kindnessMessage.message + '%20%23happyworld';
-        $log.debug(random, response.data[random]);
-      }, function (err) {
-        $log.debug('err', err);
-      });
+    vm.fadeOut = true;
+    vm.fadeIn = false;
+    $timeout(function () {
+      $http
+        .get('./app/techs/kindnessMessages.json')
+        .then(function (response) {
+          $log.debug('here', response);
+          vm.fadeIn = true;
+          vm.fadeOut = false;
+          var random = Math.floor(Math.random() * 5) + 1;
+          vm.kindnessMessage = response.data[random];
+          vm.url = 'https://twitter.com/intent/tweet?text=I did a random act of kindness today. ' + vm.kindnessMessage.message + '%20%23happyworld';
+          $log.debug(random, response.data[random]);
+        }, function (err) {
+          $log.debug('err', err);
+        });
+    }, 1000);
   };
   vm.getAct();
   vm.newAct = function () {
